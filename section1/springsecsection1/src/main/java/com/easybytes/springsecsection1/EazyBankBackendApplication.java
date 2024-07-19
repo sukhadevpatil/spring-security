683f9f80d9bf871669936179a8552c982ef6a685
Sukhdev Patil <sukhadev.patil@gmail.com>
2024-07-19 23:48:34 +0530

    spring security changes

--- section1/springsecsection1/src/main/java/com/easybytes/springsecsection1/EazyBankBackendApplication.java
+++ section1/springsecsection1/src/main/java/com/easybytes/springsecsection1/EazyBankBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springsecsection1Application {
+public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springsecsection1Application.class, args);
+		SpringApplication.run(EazyBankBackendApplication.class, args);
 	}
 
 }
---
+++ section1/springsecsection1/src/main/java/com/easybytes/springsecsection1/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.easybytes.springsecsection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Eazy Banking World with security";
+    }
+}
